df9b8badc76c518a47ba172030cebf020706d9fb
lanre <lanre@gmail.com>
2023-02-22 15:57:20 -0800

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print all alphabet in lowercase
+ * Return: Void
+ */
+
+	void print_alphabet(void)
+	{
+		char letter;
+
+
+		for (letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
+
+
+		_putchar('\n');
+	}
+
